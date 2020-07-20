51979fb63f5303ad263abef0a49b8f9f56978de7
David Felix <david.felix02@hotmail.com>
2020-07-20 19:52:33 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -6,7 +6,7 @@ end
 
 def list_dwarves(array)
  
-  array.each_with_index do |character,position+1|
-    puts "#{position}. Hello #{character}!"
+  array.each_with_index do |character,position|
+    puts "#{position+1}. Hello #{character}!"
   end
 end
